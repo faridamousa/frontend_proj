48c3b1ee24362aca4c73c4a4c1363cd77eee7d70
Abdelrahman Amr Eweis <abdelrahmanamreweis@gmail.com>
2024-07-17 13:05:32 +0300

    editted cards

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,26 +1,3 @@
-// import { Box } from "@mui/material";
-// import { Character } from "./interfaces/Character";
-
-// export const Card = ({name, status, image }: Character) => {
-//   const styles = {
-//     container: {
-//       display: "flex",
-//       flexDirection: "column",
-//       padding: "20px",
-//       borderRadius: "20px",
-//       width: "20%",
-//       backgroundColor: "#f0f0f0",
-//     },
-//   };
-//   return (
-//     <Box sx={styles.container}>
-//       <img src={image} width="50" height="50"></img>
-//       <p>{name}</p>
-//       <p>{status}</p>
-//     </Box>
-//   );
-// };
-
 import { Card } from "@mui/material";
 import { Character } from "./interfaces/Character";
 import { useNavigate } from "react-router-dom";
@@ -42,6 +19,6 @@ export const CharacterCard = ({ name, status, image, id }: Character) => {
       <img src={image} width="50" height="50"></img>
       <p>{name}</p>
       <p>{status}</p>
-    </Card>
-  );
+    </Card>
+  );
 };
